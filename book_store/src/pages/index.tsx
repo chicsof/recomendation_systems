e94ce3be85cc296e6cf57a284d69fd3441cabdbe
06kellyjac <dev@j-k.io>
2019-04-08 15:16:28 +0100

    Added @emotion/core and started adding a loading spinner

--- book_store/src/pages/index.tsx
+++ book_store/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+import { BarLoader } from "react-spinners";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -14,6 +15,17 @@ if (process.env.NODE_ENV !== "production") {
 	localStorage.setItem("debug", "book_store:*");
 }
 
+const loaderStyle: React.CSSProperties = {
+	background: "#DDD",
+	borderRadius: "5px",
+	boxShadow: "0px 0px 0px 500vh rgba(0,0,0,0.25)",
+	left: "50%",
+	padding: "15px",
+	position: "fixed",
+	top: "50%",
+	transform: "translate(-50%, -50%)",
+};
+
 const IndexPage = () => (
 	<IndexLayout>
 		<Page>
@@ -21,6 +33,11 @@ const IndexPage = () => (
 			<Container>
 				<BookBuyer />
 				<BookRater />
+				{/*
+				<div style={loaderStyle}>
+					<BarLoader height={4} width={245} color={"#87c64f"} />
+				</div>
+				*/}
 			</Container>
 		</Page>
 	</IndexLayout>
