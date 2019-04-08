660ca5b37543535efb50b52f6beda9845d8142ec
06kellyjac <dev@j-k.io>
2019-04-08 13:40:59 +0100

    Fixed depracated usages of react-emotion

--- book_store/src/components/Container.tsx
+++ book_store/src/components/Container.tsx
@@ -1,5 +1,5 @@
+import styled from "@emotion/styled";
 import * as React from "react";
-import styled from "react-emotion";
 
 import { getEmSize } from "../styles/mixins";
 import { widths } from "../styles/variables";
