79e383ff4365e0be6a2db4a1a71668aaccaa5e1b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-09 18:26:35 +0000

    Deploy book-rating.shop to github.com/chicsof/recomendation_systems.git:gh-pages

---
+++ styles-771efbc70676ca04263c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{166:function(n,o,w){},168:function(n,o,w){},217:function(n,o,w){}}]);
+//# sourceMappingURL=styles-771efbc70676ca04263c.js.map
